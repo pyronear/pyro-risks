886d89450f1c69d3d9446c6951c7dc6e0a0eac1f
F-G Fernandez <fgfm03@hotmail.fr>
2020-10-01 11:18:09 +0200

    feat: Initial repo setup (#1)
    * feat: Added empty Python project
    
    * chore: Added setup for python package
    
    * docs: Added sphinx documentation setup
    
    * style: Added flake8 config
    
    * chore: Added coverage config
    
    * chore: Added CI jobs for unittest, doc build, and lint check
    
    * test: Added template unittest
    
    * chore: Fixed setup script
    
    * test: Fixed unittest

---
+++ test/test_core.py
@@ -0,0 +1,12 @@
+import unittest
+import pyronear_ds
+
+
+class CoreTester(unittest.TestCase):
+    # Template unittest
+    def test_version(self):
+        self.assertEqual(len(pyronear_ds.__version__.split('.')), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
