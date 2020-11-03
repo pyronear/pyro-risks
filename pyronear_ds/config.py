0e0e0bd74a234f56893c734a4ee8f9d6f291900b
F-G Fernandez <fgfm03@hotmail.fr>
2020-11-03 19:38:06 +0100

    feat: Added geoJSON retrieval function

---
+++ pyronear_ds/config.py
@@ -0,0 +1,4 @@
+FR_GEOJSON: str = "https://france-geojson.gregoiredavid.fr/repo/departements.geojson"
+FR_GEOJSON_FALLBACK: str = "https://github.com/pyronear/pyronear-datascience/releases/download/v0.1.0-data/departements.geojson"
+FR_FIRES_FALLBACK: str = "https://github.com/pyronear/pyronear-datascience/releases/download/v0.1.0-data/export_BDIFF_incendies_20201027.csv"
+FR_WEATHER_FALLBACK: str = "https://github.com/pyronear/pyronear-datascience/releases/download/v0.1.0-data/noaa_weather_20201025.csv"
