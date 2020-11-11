bf18350aa77645d63bb3397815ccf67e0d742971
Milton Minervino <mminervino@quantmetry.com>
2020-11-11 21:53:03 +0100

    config: add fwi source path, repo, root and data paths

--- pyronear_ds/config.py
+++ pyronear_ds/config.py
@@ -1,6 +1,14 @@
+import os
+
 FR_GEOJSON: str = "https://france-geojson.gregoiredavid.fr/repo/departements.geojson"
 DATA_FALLBACK: str = "https://github.com/pyronear/pyronear-datascience/releases/download/v0.1.0-data"
 FR_GEOJSON_FALLBACK: str = f"{DATA_FALLBACK}/departements.geojson"
 FR_FIRES_FALLBACK: str = f"{DATA_FALLBACK}/export_BDIFF_incendies_20201027.csv"
 FR_WEATHER_FALLBACK: str = f"{DATA_FALLBACK}/noaa_weather_20201025.csv"
 FR_NASA_FIRMS_FALLBACK: str = f"{DATA_FALLBACK}/NASA_FIRMS.json"
+FR_FWI_2019_FALLBACK: str = f"{DATA_FALLBACK}/JRC_FWI_2019.zip"
+FR_FWI_2020_FALLBACK: str = f"{DATA_FALLBACK}/JRC_FWI_2020.zip"
+
+REPO_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(REPO_DIR, '.data/')
