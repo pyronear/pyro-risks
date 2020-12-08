7644d64a6dba28ad9706d238af21c853c2b0a471
Milton Minervino <milminervino@gmail.com>
2020-12-08 16:18:41 +0100

    add example for score_v0 new features

---
+++ scripts/example_scorev0.py
@@ -0,0 +1,21 @@
+from pyro_risks.datasets import ModelDF
+from pyro_risks.models.score_v0 import (
+    add_lags, prepare_dataset, train_random_forest, split_train_test, xgb_model
+)
+
+SELECTED_DEP = ['Pyrénées-Atlantiques', 'Hautes-Pyrénées', 'Ariège', 'Haute-Corse',
+                'Lozère', 'Gard', 'Hérault', 'Bouches-du-Rhônes', 'Pyrénées-Orientales',
+                'Cantal', 'Alpes-Maritimes', 'Aveyron']
+
+
+def run():
+    df = ModelDF()
+    df_lags = add_lags(df, df.drop(['day', 'departement', 'fires'], axis=1).columns)
+    X, y = prepare_dataset(df_lags, selected_dep=SELECTED_DEP)
+    X_train, X_test, y_train, y_test = split_train_test(X, y)
+    train_random_forest(X_train, X_test, y_train, y_test)
+    xgb_model(X_train, y_train, X_test, y_test)
+
+
+if __name__ == "__main__":
+    run()
