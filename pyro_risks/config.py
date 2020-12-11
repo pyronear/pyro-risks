62c4c05a4b1d1ea20d69ec95f17ba953cb5183ed
Milton Minervino <milminervino@gmail.com>
2020-12-11 12:25:50 +0100

    changed config to include model params

--- pyro_risks/config.py
+++ pyro_risks/config.py
@@ -24,3 +24,40 @@ TEST_FWI_FALLBACK: str = f"{DATA_FALLBACK}/test_data_FWI.csv"
 REPO_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(REPO_DIR, ".data/")
+
+RFMODEL_PATH: str = f"{DATA_FALLBACK}/pyrorisk_rfc_111220.pkl"
+XGBMODEL_PATH: str = f"{DATA_FALLBACK}/pyrorisk_xgb_091220.pkl"
+
+MODEL_VARIABLES = ['ffmc_min', 'str_mean', 'str_min', 'str_max', 'ffmc_mean', 'isi_min',
+                   'ffmc_min_lag1', 'strd_mean', 'isi_mean', 'strd_min', 'strd_max',
+                   'rsn_max', 'ffmc_mean_lag1', 'rsn_max_lag1', 'str_mean_lag1',
+                   'str_min_lag1', 'ffmc_std', 'ffmc_max', 'rsn_std', 'str_max_lag1',
+                   'rsn_std_lag1', 'rsn_max_lag3', 'isi_min_lag1', 'isi_mean_lag1',
+                   'ffmc_max_lag1', 'rsn_std_lag3', 'stl1_std_lag1', 'stl1_std', 'isi_max',
+                   'strd_min_lag1', 'ffmc_min_lag3', 'ffmc_std_lag1', 'strd_mean_lag1',
+                   'rsn_mean_lag1', 'fwi_mean', 'isi_max_lag1', 'sd_max', 'strd_max_lag1',
+                   'rsn_mean', 'snowc_std_lag7', 'stl1_std_lag3']
+TRAIN_SELECTED_DEP = ['Aisne', 'Alpes-Maritimes', 'Ardèche', 'Ariège', 'Aude', 'Aveyron',
+                      'Cantal', 'Eure', 'Eure-et-Loir', 'Gironde', 'Haute-Corse', 'Hautes-Pyrénées',
+                      'Hérault', 'Indre', 'Landes', 'Loiret', 'Lozère', 'Marne', 'Oise',
+                      'Pyrénées-Atlantiques', 'Pyrénées-Orientales', 'Sarthe', 'Somme', 'Yonne']
+
+RF_PARAMS = {
+    "n_estimators": 500,
+    "min_samples_leaf": 10,
+    "max_features": 'sqrt',
+    "class_weight": "balanced",
+    "criterion": 'gini',
+    "random_state": 10,
+    "n_jobs": -1
+}
+
+XGB_PARAMS = {
+    'max_depth': 10,
+    'min_child_weight': 10,
+    'eta': .01,
+    'subsample': .8,
+    'colsample_bytree': .8,
+    'objective': 'binary:logistic',
+    'eval_metric': ['logloss', 'aucpr']
+}
