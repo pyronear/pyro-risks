4796754d3a8fe1d34976bf68ffc852eb006f3ae7
Milton Minervino <milminervino@gmail.com>
2020-12-15 10:12:03 +0100

    Modify unitary tests

--- test/test_models.py
+++ test/test_models.py
@@ -79,13 +79,13 @@ class ModelsTester(unittest.TestCase):
     def test_pyrorisk(self):
         pr = predict.PyroRisk(which='RF')
         self.assertEqual(pr.model.n_estimators, 500)
-        self.assertEqual(pr.model_path, cfg.RFMODEL_PATH)
+        self.assertEqual(pr.model_path, cfg.RFMODEL_ERA5T_PATH)
         res = pr.get_input('2020-05-05')
         self.assertIsInstance(res, pd.DataFrame)
-        self.assertEqual(res.shape, (93, 41))
+        self.assertEqual(res.shape, (93, 40))
         preds = pr.predict('2020-05-05')
         self.assertEqual(len(preds), 93)
-        self.assertEqual(preds['Ardennes'], {'score': 0.11, 'explainability': None})
+        self.assertEqual(preds['Ardennes'], {'score': 0.246, 'explainability': None})
 
 
 if __name__ == "__main__":
