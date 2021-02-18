de8b616d92ae3968a663ad987d9990fa2b4b9eeb
frgfm <fgfm03@hotmail.fr>
2021-02-18 19:07:45 +0100

    chore: Added copyright & license headers

--- scripts/example_ERA5_VIIRS.py
+++ scripts/example_ERA5_VIIRS.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2021, Pyronear contributors.
+
+# This program is licensed under the GNU Affero General Public License version 3.
+# See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.
+
 from pyro_risks.datasets import NASAFIRMS_VIIRS, ERA5Land
 from pyro_risks.datasets.datasets_mergers import (
     merge_datasets_by_departements,
