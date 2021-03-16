d7a9fd6a353f7f9d67857782bda78d0d0f57a9fa
Joshua Sant'Anna <45068597+dataJSA@users.noreply.github.com>
2021-03-16 16:52:07 +0100

    chore: Add copyright license headers

--- test/test_load.py
+++ test/test_load.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2021, Pyronear contributors.
+
+# This program is licensed under the GNU Affero General Public License version 3.
+# See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.
+
 from pyro_risks.load import load_dataset
 from pyro_risks import load
 from unittest import mock
