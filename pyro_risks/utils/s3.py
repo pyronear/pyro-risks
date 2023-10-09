b3c805e037bb580e19797d786ce39e48123aa0b6
Jules <101271502+juldpnt@users.noreply.github.com>
2023-10-09 21:07:26 +0200

    S3 enhanced (#70)
    * refactor: s3 submodule methods modification
    
    * add: notebook to show how to interact with aws s3

--- pyro_risks/utils/s3.py
+++ pyro_risks/utils/s3.py
@@ -1,4 +1,5 @@
 import boto3
+import os
 
 __all__ = ["S3Bucket"]
 
@@ -30,6 +31,10 @@ class S3Bucket:
 
         >>> s3.download_file('path/to/my_file.txt', 'my_downloaded_file.txt')
 
+        To download a folder from the bucket, use:
+
+        >>> s3.download_folder('path/to/my_folder', 'my_downloaded_folder')
+
         To delete a file from the bucket, use:
 
         >>> s3.delete_file('path/to/my_file.txt')
@@ -40,11 +45,15 @@ class S3Bucket:
 
         To filter files by a pattern, use:
 
-        >>> pattern_files = s3.list_files(pattern='my_prefix')
+        >>> pattern_files = s3.list_files(patterns=["pattern1", "pattern2"])
 
         To get metadata for a file in the bucket, use:
 
         >>> metadata = s3.get_file_metadata('path/to/my_file.txt')
+
+        To get file size and last modified date for some files in the bucket, use:
+
+        >>> files_metadata = s3.get_files_metadata(prefix="path/to/my_folder_or_file")
     """
 
     def __init__(
@@ -74,6 +83,7 @@ class S3Bucket:
         self.session = boto3.Session(**session_args)
         self.s3 = self.session.resource("s3", endpoint_url=endpoint_url)
         self.bucket = self.s3.Bucket(bucket_name)
+        self.bucket_name = bucket_name
 
     def upload_file(self, file_path: str, object_key: str) -> None:
         """
@@ -95,6 +105,21 @@ class S3Bucket:
         """
         self.bucket.download_file(object_key, file_path)
 
+    def download_folder(self, prefix: str, save_path: str = "") -> None:
+        """
+        Downloads a folder from the S3 bucket.
+
+        Args:
+            prefix (str): The S3 key (path) of the folder to download.
+            save_path (str, optional): The local folder where the folder tree will be saved.
+        """
+        if save_path != "" and not (save_path.endswith("/")):
+            save_path += "/"
+        for obj in self.bucket.objects.filter(Prefix=prefix):
+            if not os.path.exists(os.path.dirname(save_path + obj.key)):
+                os.makedirs(os.path.dirname(save_path + obj.key))
+            self.bucket.download_file(obj.key, save_path + obj.key)
+
     def delete_file(self, object_key: str) -> None:
         """
         Deletes a file from the S3 bucket.
@@ -104,19 +129,43 @@ class S3Bucket:
         """
         self.bucket.Object(object_key).delete()
 
-    def list_files(self, pattern: str = None) -> list[str]:
+    def list_folders(self, prefix: str = "", delimiter: str = "") -> list[str]:
+        """
+        Lists folders in the S3 bucket.
+
+        Args:
+            prefix (str, optional): Only folders with keys starting with this prefix will be listed.
+            delimiter (str, optional): The delimiter to use for the folder listing.
+
+        Returns:
+            A list of folder keys (paths) in the bucket.
+        """
+        folders = []
+        for obj in self.bucket.meta.client.list_objects_v2(
+            Bucket=self.bucket_name, Prefix=prefix, Delimiter=delimiter
+        )["CommonPrefixes"]:
+            folders.append(obj["Prefix"])
+        return folders
+
+    def list_files(
+        self, patterns: list[str] = None, prefix: str = "", delimiter: str = ""
+    ) -> list[str]:
         """
         Lists files in the S3 bucket.
 
         Args:
-            pattern (str): The pattern to filter files by (optional).
+            patterns (list[str], optional): Only files with keys containing one of the patterns will be listed.
+            prefix (str, optional): Only folders with keys starting with this prefix will be listed.
+            delimiter (str, optional): The delimiter to use for the folder listing.
 
         Returns:
             A list of file keys (paths) in the bucket.
         """
         files = []
-        for obj in self.bucket.objects.all():
-            if not pattern or pattern in obj.key:
+        for obj in self.bucket.objects.filter(Prefix=prefix, Delimiter=delimiter):
+            if not patterns or (
+                type(patterns) == list and any([p in obj.key for p in patterns])
+            ):
                 files.append(obj.key)
         return files
 
@@ -134,24 +183,62 @@ class S3Bucket:
         metadata = obj.metadata
         return metadata
 
-    def get_files_metadata(self, pattern: str = None) -> dict:
+    def get_files_metadata(
+        self, patterns: list[str] = None, prefix: str = "", delimiter: str = ""
+    ) -> list[dict]:
         """
         Lists files in the S3 bucket with their size in bytes and last modified dates.
 
         Args:
-            pattern (str): The pattern to filter files by (optional).
+            patterns (list[str], optional): Only files with keys containing one of the patterns will be listed.
+            prefix (str, optional): Only folders with keys starting with this prefix will be listed.
+            delimiter (str, optional): The delimiter to use for the folder listing.
 
         Returns:
             A dictionnary of file keys (paths), file sizes en GB and last modified dates in the bucket.
         """
         files = []
-        for obj in self.bucket.objects.all():
-            if not pattern or pattern in obj.key:
+        for obj in self.bucket.objects.filter(Prefix=prefix, Delimiter=delimiter):
+            if not patterns or (
+                type(patterns) == list and any([p in obj.key for p in patterns])
+            ):
                 files.append(
                     {
                         "file_name": obj.key,
-                        "file_size": round(obj.size * 1.0 / (1024), 2), 
+                        "file_size": round(obj.size * 1.0 / (1024), 2),
                         "file_last_modified": obj.last_modified,
                     }
                 )
         return files
+
+
+def read_credentials(
+    credentials_path: str,
+) -> dict:
+    """
+    Retrieves credentials from a file.
+
+    Args:
+        credentials_path (str): The path of the file containing the credentials.
+
+    Returns:
+        A dictionary containing the credentials for aws s3 access.
+    """
+    credentials = {}
+    with open(credentials_path + "credentials", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if "aws_access_key_id" in line:
+                credentials["aws_access_key_id"] = line.split("=")[1].strip()
+            if "aws_secret_access_key" in line:
+                credentials["aws_secret_key"] = line.split("=")[1].strip()
+
+    with open(credentials_path + "config", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if "region" in line:
+                credentials["region_name"] = line.split("=")[1].strip()
+    credentials["endpoint_url"] = (
+        "https://s3." + credentials["region_name"] + ".io.cloud.ovh.net/"
+    )
+    return credentials
