dbce0a60da7f8291a8541a3700d62f931ad23b32
Alexis Cruveiller <33450491+Acruve15@users.noreply.github.com>
2023-11-13 18:39:08 +0100

    Update main.py
    use environment variables for connection

--- pyro_risks/platform_fwi/main.py
+++ pyro_risks/platform_fwi/main.py
@@ -1,25 +1,30 @@
+# Usual Imports
+import os
 from datetime import date
+from dotenv import load_dotenv
+
+# Pyro Risks Imports
 from pyro_risks.utils.fwi_helpers import FWIHelpers
 from pyro_risks.utils.s3 import S3Bucket
 
 
 def main():
+    load_dotenv()
     # Create an S3Bucket instance
     s3 = S3Bucket(
-        bucket_name="pyro-risk",
-        endpoint_url="http://s3.localhost.localstack.cloud:4566",
-        region_name="gra",
-        aws_access_key_id="fake",
-        aws_secret_key="fake",
+        bucket_name=os.environ["BUCKET_NAME"],
+        endpoint_url=os.environ["ENDPOINT_URL"],
+        region_name=os.environ["REGION_NAME"],
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
+        aws_secret_key=os.environ["AWS_SECRET_KEY"],
     )
 
+    # Get the FWI GeoJSON from EFFIS
     today_date_str_url = date.today().strftime("%Y-%m-%d")
     effis_tiff_file_url = (
         "https://ies-ows.jrc.ec.europa.eu/effis?LAYERS=ecmwf007.fwi&FORMAT=image/tiff&TRANSPARENT=true&SINGLETILE=false&SERVICE=wms&VERSION=1.1.1&REQUEST=GetMap&STYLES=&SRS=EPSG:4326&BBOX=-6.0,41.0,10.0,52.0&WIDTH=1600&HEIGHT=1200&TIME="
         + today_date_str_url
     )
-
-    today_date_str_url = date.today().strftime("%Y_%m_%d")
 
     # Download file from EFFIS and convert it to a geodf
     fwi = FWIHelpers()
@@ -32,13 +37,12 @@ def main():
 
     new_json_fwi = fwi.fwi_geojson_maker(gdf_fwi)
 
-    today_date_str_url = date.today().strftime("%Y_%m_%d")
-    filepath_in_bucket = "fwi/fwi_" + today_date_str_url + ".geojson"
-
-    # Upload the JSON data to Github
-
     # Store the JSON data to S3
-    s3.write_json_to_s3(object_key=filepath_in_bucket, json_data=new_json_fwi)
+    year, month, day = today_date_str_url.split("-")
+    s3.write_json_to_s3(
+        object_key=f"fwi/year={year}/month={month}/day={day}/fwi_values.json",
+        json_data=new_json_fwi,
+    )
 
 
 if __name__ == "__main__":
