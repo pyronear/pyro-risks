fd15de0b253206f19dcd3b12bcfe52aeb8e601a3
Alexis Cruveiller <33450491+Acruve15@users.noreply.github.com>
2023-11-28 08:18:40 +0100

    Update main.py
    - restructure task
    - add retrieved_date option

--- pyro_risks/platform_fwi/main.py
+++ pyro_risks/platform_fwi/main.py
@@ -1,4 +1,5 @@
 # Usual Imports
+import click
 import os
 from datetime import date
 from dotenv import load_dotenv
@@ -8,22 +9,20 @@ from pyro_risks.utils.fwi_helpers import FWIHelpers
 from pyro_risks.utils.s3 import S3Bucket
 
 
-def main():
-    load_dotenv()
-    # Create an S3Bucket instance
-    s3 = S3Bucket(
-        bucket_name=os.environ["BUCKET_NAME"],
-        endpoint_url=os.environ["ENDPOINT_URL"],
-        region_name=os.environ["REGION_NAME"],
-        aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
-        aws_secret_key=os.environ["AWS_SECRET_KEY"],
-    )
-
+@click.command()
+@click.option(
+    "--retrieved-date",
+    type=str,
+    default=None,
+    help="Date to retrieve the FWI data from EFFIS. Format: YYYY-MM-DD.",
+)
+def main(retrieved_date):
     # Get the FWI GeoJSON from EFFIS
-    today_date_str_url = date.today().strftime("%Y-%m-%d")
+    if retrieved_date is None:
+        retrieved_date = date.today().strftime("%Y-%m-%d")
     effis_tiff_file_url = (
         "https://ies-ows.jrc.ec.europa.eu/effis?LAYERS=ecmwf007.fwi&FORMAT=image/tiff&TRANSPARENT=true&SINGLETILE=false&SERVICE=wms&VERSION=1.1.1&REQUEST=GetMap&STYLES=&SRS=EPSG:4326&BBOX=-6.0,41.0,10.0,52.0&WIDTH=1600&HEIGHT=1200&TIME="
-        + today_date_str_url
+        + retrieved_date
     )
 
     # Download file from EFFIS and convert it to a geodf
@@ -38,7 +37,18 @@ def main():
     new_json_fwi = fwi.fwi_geojson_maker(gdf_fwi)
 
     # Store the JSON data to S3
-    year, month, day = today_date_str_url.split("-")
+
+    load_dotenv()
+
+    s3 = S3Bucket(
+        bucket_name=os.environ["BUCKET_NAME"],
+        endpoint_url=os.environ["ENDPOINT_URL"],
+        region_name=os.environ["REGION_NAME"],
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
+        aws_secret_key=os.environ["AWS_SECRET_KEY"],
+    )
+
+    year, month, day = retrieved_date.split("-")
     s3.write_json_to_s3(
         object_key=f"fwi/year={year}/month={month}/day={day}/fwi_values.json",
         json_data=new_json_fwi,
@@ -46,4 +56,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main()  # pragma: no cover
