dd927147e8f5e033e5d2c278e23cf8cf490b701b
Joshua Sant'Anna <45068597+jsakv@users.noreply.github.com>
2024-07-02 18:24:36 +0200

    chore: Add Mock predictor

--- app/api/inference.py
+++ app/api/inference.py
@@ -3,10 +3,13 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-from pyro_risks.models.predict import PyroRisk
-
 
 __all__ = ["predictor"]
 
 
-predictor = PyroRisk(which="RF")
+class Mock:
+    def predict(self, date):
+        return {"01": 0.5, "02": 0.5}
+
+
+predictor = Mock()
